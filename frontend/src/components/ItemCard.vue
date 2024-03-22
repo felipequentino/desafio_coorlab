e203ff4588efdf0fd293cde63c3e08d52c46c2e4
felipequentino <felipequentino22@gmail.com>
2024-03-22 17:35:05 -0300

    Component card to show the data searched

---
+++ frontend/src/components/ItemCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="item-card">
+      <h2>{{ item.name }}</h2>
+      <p>{{ item.city }}</p>
+      <p>{{ item.date }}</p>
+      <!-- Adicione mais campos conforme necessário -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['item'],
+    name: 'ItemCard'
+  };
+  </script>
+  
+  <style scoped>
+  .item-card {
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin-bottom: 20px;
+  }
+  </style>
