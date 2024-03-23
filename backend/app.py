ce93fa6641e1aa60e1a24419900271f3b509ae22
felipequentino <felipequentino22@gmail.com>
2024-03-22 22:57:50 -0300

    Finished the Flask API
    This file open the data.json, and define a rote to the API, that only accepts GET solicitations, and when this rote is acessed (by ItemManagement.vue), return the itens off 'arr' em Json format. Also, start the app flask on 3000 door

--- backend/app.py
+++ backend/app.py
@@ -13,33 +13,10 @@ arr = []
 for item in data["transport"]:
     arr.append(item)
 
-print(arr[4]["city"])
 
 @app.route('/api/items', methods=['GET'])
 def get_data():
     return jsonify(arr)
 
-@app.route('/api/items', methods=['POST'])
-def create_item():
-    new_item = request.json
-    arr.append(new_item)
-    return jsonify(new_item), 201
-
-@app.route('/api/items/<int:item_id>', methods=['PUT'])
-def update_item(item_id):
-    for item in data:
-        if item['id'] == item_id:
-            item.update(request.json)
-            return jsonify(item)
-    return jsonify({'error': 'Item not found'}), 404
-
-@app.route('/api/items/<int:item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    for index, item in enumerate(data):
-        if item['id'] == item_id:
-            del data[index]
-            return jsonify({'message': 'Item deleted'})
-    return jsonify({'error': 'Item not found'}), 404
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
