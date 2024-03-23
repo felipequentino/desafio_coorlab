b0f3e3aec577aa2cfd0478ead1d539b581f04bf0
felipequentino <felipequentino22@gmail.com>
2024-03-22 22:52:40 -0300

    Configs off App finished
    Imports off PrimeVue and components finalized, parameters to components ItemManagement and CityFilter setted, CSS config to displays with max-width:600px.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,70 @@
 <template>
-  <div id="app">
-    <h1>CB Viagens</h1>
-    <img alt="CB logo" src="../../doc/logo.png">
-    <CityFilter />
-    <ItemManagement />
+  <div id="app" class="flex-container">
+    <Sidebar />
+    <div class="main-content">
+      <h1 id="header">Calcule o valor da Viagem</h1>
+      <ItemManagement @itemAdded="updateItems" />
+      <CityFilter :items="items" />
+    </div>
   </div>
 </template>
 
 <script>
 import CityFilter from './components/CityFilter.vue';
 import ItemManagement from './components/ItemManagement.vue';
+import Sidebar from './components/Sidebar.vue';
+
+import 'primevue/resources/themes/saga-blue/theme.css'; // theme
+import 'primevue/resources/primevue.min.css'; // css 
+import 'primeicons/primeicons.css'; // icons
 
 export default {
+  name: 'App',
   components: {
     CityFilter,
-    ItemManagement
+    ItemManagement,
+    Sidebar
   },
   data() {
     return {
       items: [],
-      newItemName: ''
     };
   },
+  methods: {
+    updateItems(items) {
+      this.items = items;
+    }
+  }
 };
 </script>
 
 <style>
 #app {
   font-family: Arial, Helvetica, sans-serif;
+}
+
+#id {
+  margin-left: 260px
+}
+
+#header {
+  margin-left: 255px;
+}
+.main-content {
+  margin-left: 20px; 
+  width: 100%;
+}
+body, html {
+  margin: 0;
+  padding: 0;
+}
+
+@media (max-width: 600px) {
+  .main-content {
+    margin-left: 0px;
+  }
+  #header {
+    margin-left: 5px;
+  }
 }
 </style>
