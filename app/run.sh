3a8737bae3771f922ba57bcde80318a95e8dfbea
felipequentino <felipequentino22@gmail.com>
2024-03-22 23:20:27 -0300

    Steps to run the project

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+# go to the backend directory
+cd /backend/
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
+pip install flask flask-cors
+
+# Run the backend
+python3 app.py
+
+# go back to the frontend directory
+cd ../frontend/
+
+# Install dependencies
+npm install axios primevue@latest --save primeicons@latest --save autoprefixer
+
+# Run the frontend
+npm run serve
