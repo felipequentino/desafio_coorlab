1176e3e689c30504151b51124567d2d448be0e83
felipequentino <felipequentino22@gmail.com>
2024-03-22 22:44:17 -0300

    Finish ItemCard Component
    This component is a card to show the data, making it easier to work and visualize several informations

--- frontend/src/components/ItemCard.vue
+++ frontend/src/components/ItemCard.vue
@@ -1,23 +1,51 @@
 <template>
-    <div class="item-card">
-      <h2>{{ item.name }}</h2>
-      <p>{{ item.city }}</p>
-      <p>{{ item.date }}</p>
-      <!-- Adicione mais campos conforme necessário -->
+  <div class="item-card">
+    <h2>{{ item.name }}</h2>
+    <p>{{ item.city }}</p>
+    <p>{{ item.duration}}</p>
+
+    <div class="sub-card">
+      <i class="pi pi-clock"></i> <span class="type">Conforto</span>
+      <p>Poltrona disponível: {{ item.seat }}</p>
+      <p>Preço: {{ item.price_econ }}</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: ['item'],
-    name: 'ItemCard'
-  };
-  </script>
-  
-  <style scoped>
-  .item-card {
-    border: 1px solid #ccc;
-    padding: 20px;
-    margin-bottom: 20px;
+
+    <div class="sub-card">
+      <i class="pi pi-dollar"></i> <span class="type"> Econômica</span>
+      <p>Cama disponível: {{ item.bed }}</p>
+      <p>Preço: {{ item.price_confort }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  name: 'ItemCard'
+};
+</script>
+
+<style scoped>
+  .type {
+    font-family: Arial, Helvetica, sans-serif;
   }
-  </style>
+  .item-card {
+      flex: 1 0 240px; 
+      margin: 10px;
+      border: 1px solid #000000;
+      background-color: #2196f3;
+      padding: 20px;
+      margin-bottom: 20px;
+  }
+  @media (max-width: 600px) {
+    .item-card {
+      flex: 1 0 95%;
+    }
+  }
+  .sub-card {
+      border: 1px solid #000000;
+      background-color: #f1f1f1;
+      padding: 10px;
+      margin: 10px 0;
+  }
+</style>
