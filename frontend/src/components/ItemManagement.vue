d8886a7596d3e6fc45cd7a40dba73280f939be71
felipequentino <felipequentino22@gmail.com>
2024-03-23 00:01:34 -0300

    Finish component ItemManagement.vue
    This component receives the info from the API off Flask, the info is captured in App.vue, line 6. This data is shared by using '$emit'.

--- frontend/src/components/ItemManagement.vue
+++ frontend/src/components/ItemManagement.vue
@@ -2,14 +2,17 @@
 import axios from 'axios';
 
 export default {
+    // data to store the items
     data() {
         return {
             items: [],
         };
     },
+    // get items when component is created
     created() {
         this.getItems(); 
     },
+    // GET method to get items
     methods: {
         getItems() {
             axios.get('http://localhost:3000/api/items')
