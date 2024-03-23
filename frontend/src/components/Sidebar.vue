cf7f9fb9110b29874c841b1d502293355e917445
felipequentino <felipequentino22@gmail.com>
2024-03-22 22:49:53 -0300

    Finish component Sidebar
    This component is just a Sidebar, showing the image Logo of the company. In mobile devices, the sidebar display is set to 0, so, isn't shown.

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="sidebar">
+    <img alt="CB logo" src="../../../doc/logo.png" class="sidebar-logo">
+    <br><br>
+    <i class="pi pi-calculator"> Calculadora</i> 
+</div>
+</template>
+
+<script>
+    import 'primeicons/primeicons.css';
+    export default {
+        name: 'SideBar'
+    };
+</script>
+
+<style scoped>
+.sidebar {
+    position: fixed; /* ou absolute */
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 229px;
+    background-color: #3e3d5a;
+    padding: 20px;
+    box-sizing: border-box;
+}
+
+.sidebar-logo {
+    width: 100%;
+}
+
+.sidebar-label {
+    color: white;
+    margin-top: 20px;
+    font-size: 18px;
+}
+
+.pi {
+    color: white;
+    margin-top: 20px;
+    font-size: 18px;
+    display: block;
+    cursor: pointer;
+}
+
+@media (max-width: 600px) {
+    .sidebar {
+        display: none;
+    }
+}
+</style>
