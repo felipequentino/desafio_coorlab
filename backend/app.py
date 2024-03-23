6898d682c28d83731ad94eab1dc7a5bfe1ffa8ed
felipequentino <felipequentino22@gmail.com>
2024-03-23 00:03:39 -0300

    Finished the Flask API
    This file open the data.json, and define a rote to the API, that only accepts GET solicitations, and when this rote is acessed (by ItemManagement.vue), return the itens off 'arr' em Json format. Also, start the app flask on 3000 door

--- backend/app.py
+++ backend/app.py
@@ -5,15 +5,17 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+# Load data from JSON file
 with open('../app/data.json') as f:
     data = json.load(f)
 
 arr = []
 
+# Append data to array
 for item in data["transport"]:
     arr.append(item)
 
-
+# GET request
 @app.route('/api/items', methods=['GET'])
 def get_data():
     return jsonify(arr)
